3dad91e778d0828f1ba0a3b966a43d7e8f5dcdde
Evgeny Proydakov <e.proydakov@rambler-co.ru>
2017-02-27 14:26:21 +0300

    Fix crash

--- sweet_lady_bot.go
+++ sweet_lady_bot.go
@@ -62,7 +62,7 @@ func main() {
     }
 
     if len(os.Args) == 3 {
-        f, err := os.OpenFile(os.Args[3], os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+        f, err := os.OpenFile(os.Args[2], os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
         if err != nil {
             log.Fatalf("error opening file: %v", err)
         }
